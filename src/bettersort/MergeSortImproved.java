fba9cd02a05571db0cf6bb46105d8da8bf28afe6
lilvuzy <helical_agape_0y@icloud.com>
2022-10-30 20:33:22 -0400

    merge sort improved

--- src/bettersort/MergeSortImproved.java
+++ src/bettersort/MergeSortImproved.java
@@ -7,7 +7,7 @@ import static bettersort.BasicSorts.insertionSubsort;
 
 public class MergeSortImproved {
 
-  private static final int MERGE_SORT_THRESHOLD = 15;
+  private static final int MERGE_SORT_THRESHOLD = 5;
   /**
    * Merge sort the provided array using an improved merge operation.
    */
