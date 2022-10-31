2e3ef5bbdbc79e82fa20bdb1a13c29d66ca17c76
lilvuzy <helical_agape_0y@icloud.com>
2022-10-30 20:28:41 -0400

    completed improved merge sorts

--- src/bettersort/MergeSortImproved.java
+++ src/bettersort/MergeSortImproved.java
@@ -7,12 +7,67 @@ import static bettersort.BasicSorts.insertionSubsort;
 
 public class MergeSortImproved {
 
-  private static final int MERGE_SORT_THRESHOLD = 100;
+  private static final int MERGE_SORT_THRESHOLD = 15;
   /**
    * Merge sort the provided array using an improved merge operation.
    */
   public static <T extends Comparable<T>> void mergeSortHalfSpace(T[] items) {
-    // TODO
+    T[] tmps = (T[]) new Comparable[items.length / 2];
+    mergeSortHalfSpace(items, tmps, 0, items.length - 1);
+  }
+
+  /**
+   * Recursive helper method for the merge sort algorithm.
+   *
+   * @param items The array to sort
+   * @param tmps Temporary array for merge operation
+   * @param left Index of the left end of the region to sort
+   * @param right Index of the right end of the region to sort.
+   */
+  private static <T extends Comparable<T>> void mergeSortHalfSpace(T[] items, T[] tmps, int left, int right) {
+    if (left < right) {
+      int mid = (left + right) / 2;
+      mergeSortHalfSpace(items, tmps, left, mid);
+      mergeSortHalfSpace(items, tmps, mid + 1, right);
+      merge(items, tmps, left, mid, right);
+    }
+  }
+
+  /**
+   * Merge two sorted sub-arrays using half the space of a traditional merge function.
+   */
+  private static <T extends Comparable<T>> void merge(T[] items, T[] tmps, int left, int mid, int right) {
+    for (int i = 0, j = left; j <= mid; j++, i++) {
+      tmps[i] = items[j];
+    }
+
+    int leftIndex = 0;
+    int rightIndex = mid + 1;
+    int mergeIndex = left;
+
+    // Compare left and right until reaching end of one subarray
+    while (rightIndex <= right && leftIndex <= mid - left) {
+      if (tmps[leftIndex].compareTo(items[rightIndex]) <= 0) {
+        items[mergeIndex] = tmps[leftIndex];
+        leftIndex++;
+      } else {
+        items[mergeIndex] = items[rightIndex];
+        rightIndex++;
+      }
+      mergeIndex++;
+    }
+
+    // Add remaining right subarray
+    while (rightIndex <= right) {
+      items[mergeIndex] = items[rightIndex];
+      rightIndex++;
+    }
+
+    // Add remaining left subarray
+    while (leftIndex <= mid - left) {
+      items[mergeIndex] = tmps[leftIndex];
+      leftIndex++;
+    }
   }
 
   /**
@@ -20,7 +75,7 @@ public class MergeSortImproved {
    * switching to insertion sort for small sub-arrays.
    */
   public static <T extends Comparable<T>> void mergeSortAdaptive(T[] items) {
-    T[] tmps = (T[]) new Comparable[items.length];
+    T[] tmps = (T[]) new Comparable[items.length / 2];
     mergeSortAdaptive(items, tmps, 0, items.length - 1);
   }
 
@@ -33,8 +88,8 @@ public class MergeSortImproved {
    * @param right index of right end of region to sort
    */
   private static <T extends Comparable<T>> void mergeSortAdaptive(T[] items, T[] tmps, int left, int right) {
-    if (items.length < MERGE_SORT_THRESHOLD) {
-      insertionSubsort(tmps, left, right);
+    if (right - left < MERGE_SORT_THRESHOLD) {
+      insertionSubsort(items, left, right);
     } else {
       int mid = (left + right) / 2;
       mergeSortAdaptive(items, tmps, left, mid);
@@ -44,46 +99,11 @@ public class MergeSortImproved {
   }
 
   /**
-   * Merge two sorted sub-arrays.
-   */
-  private static <T extends Comparable<T>> void merge(T[] items, T[] tmps, int left, int mid, int right) {
-    // Copy items to temporary array since we're about to
-    // chaotically reorganize the original.
-    for (int i = left; i <= right; ++i) {
-      tmps[i] = items[i];
-    }
-
-    int i = left;
-    int leftIndex = left;
-    int rightIndex = mid + 1;
-
-    // Compare heads until one subarray is empty.
-    for (; leftIndex <= mid && rightIndex <= right; ++i) {
-      if (tmps[leftIndex].compareTo(tmps[rightIndex]) <= 0) {
-        items[i] = tmps[leftIndex];
-        ++leftIndex;
-      } else {
-        items[i] = tmps[rightIndex];
-        ++rightIndex;
-      }
-    }
-
-    // Copy over remainder of left subarray.
-    for (; leftIndex <= mid; ++leftIndex, ++i) {
-      items[i] = tmps[leftIndex];
-    }
-
-    // Copy over remainder of right subarray.
-    for (; rightIndex <= right; ++rightIndex, ++i) {
-      items[i] = tmps[rightIndex];
-    }
-  }
-
-  /**
    * Merge sort the provided sub-array using our improved merge sort. This is the
    * fallback method used by introspective sort.
    */
   public static <T extends Comparable<T>> void mergeSubsortAdaptive(T[] items, int start, int end) {
-    // TODO
+    T[] tmps = (T[]) new Comparable[items.length / 2];
+    mergeSortAdaptive(items, tmps, start, end);
   }
 }
