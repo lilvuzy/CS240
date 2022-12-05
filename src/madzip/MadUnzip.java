92c2922f91450c961eacd992cbf816b18f42b145
lilvuzy <helical_agape_0y@icloud.com>
2022-12-05 18:05:17 -0500

    added mains

--- src/madzip/MadUnzip.java
+++ src/madzip/MadUnzip.java
@@ -4,4 +4,13 @@ package madzip;
  * MadUnzip class for madzip project.
  */
 public class MadUnzip {
+
+  /**
+   * Main method.
+   *
+   * @param args string arguments
+   */
+  public static void main(String[] args) {
+
+  }
 }
--- src/madzip/MadZip.java
+++ src/madzip/MadZip.java
@@ -4,4 +4,13 @@ package madzip;
  * MadZip class for madzip project.
  */
 public class MadZip {
+
+  /**
+   * Main method.
+   *
+   * @param args string arguments
+   */
+  public static void main(String[] args) {
+
+  }
 }
